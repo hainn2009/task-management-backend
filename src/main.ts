829543fbf81133540a13867529df5903be21f5a5
Nguyen Ngoc Hai <hainn2009@gmail.com>
2024-12-29 08:58:54 +0700

    add cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import { TransformInterceptor } from './transform.interceptor';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+    // This will enable CORS for all routes, should remove this in production
+    app.enableCors();
     app.useGlobalPipes(new ValidationPipe());
     app.useGlobalInterceptors(new TransformInterceptor());
     await app.listen(process.env.PORT ?? 3000);
